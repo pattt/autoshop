c5deba741297dbae6389279b27376c9a897d9fdd
Pavel Avdyeyev <pavel.avdyeyev@opower.com>
2017-10-25 21:42:16 +0300

    For those who can't modify config manually, Alex Gubarev ;)

---
+++ server/configfix.sed
@@ -0,0 +1,4 @@
+s/"host" => "localhost"/"host" => "mysqldump"/g
+s/"username" => "user13"/"username" => "root"/g
+s/"password" => ".*"/"password" => "root"/g
+s/"dbname" => "user13"/"dbname" => "autoShop"/g
--- server/install.sh
+++ server/install.sh
@@ -4,3 +4,8 @@ rm -rf www
 mkdir www
 cd $_
 git clone https://github.com/aleksandr-tkachuk/autoShopAPI.git .
+
+sed -i .htaccess.bk '/RewriteBase/d' ./.htaccess 
+cd ..
+sed -i config.bk -f configfix.sed ./www/config/config.php
+
